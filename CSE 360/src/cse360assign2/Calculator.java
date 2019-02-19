b2ec3a1b0b4a352e487a6270b31a9edd04d7a1bf
Eric Kim <junthemage@gmail.com>
2019-02-19 14:16:39 -0700

    Created the second version.

--- CSE 360/src/cse360assign2/Calculator.java
+++ CSE 360/src/cse360assign2/Calculator.java
@@ -28,27 +28,27 @@ public class Calculator
 	
 	public int getTotal () 
 	{
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) 
 	{
-		
+		total = total + value;
 	}
 	
 	public void subtract (int value) 
 	{
-		
+		total = total - value;
 	}
 	
 	public void multiply (int value) 
 	{
-		
+		total = total*value;
 	}
 	
 	public void divide (int value) 
 	{
-		
+		total = total/value;
 	}
 	
 	public String getHistory () 
