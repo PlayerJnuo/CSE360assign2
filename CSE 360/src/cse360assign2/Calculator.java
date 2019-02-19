7889f1a2f8df8cae9ba7e0748a9107183d5ca6b4
Eric Kim <junthemage@gmail.com>
2019-02-19 14:04:38 -0700

    Initial version, no change to original code.

---
+++ CSE 360/src/cse360assign2/Calculator.java
@@ -0,0 +1,58 @@
+/**	
+ *	Name: Eric Kim
+ *	ClassID: 438
+ *	Assignment #: 2
+ *	Description: The Calculator class contains the functionalies
+ *				 of a calculator. Among these are add, subtract
+ *				 divide, multiply, getTotal, and getHistory.
+ *				 Besides the +,-,%,* functions, getTotal returns
+ *				 the current amount, and getHistory returns the
+ *				 history of the inputs and outputs so far.
+ */
+
+package cse360assign2;
+
+public class Calculator 
+{
+
+	private int total;
+	
+	/**
+	 * Constructor for Calculator.
+	 * total value starts at 0.
+	 */
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	public void divide (int value) 
+	{
+		
+	}
+	
+	public String getHistory () 
+	{
+		return "";
+	}
+}
